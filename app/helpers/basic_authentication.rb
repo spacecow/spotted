553ddb6b495ca97071ced4b5eb390ee98997fc29
Johan Sveholm <jsveholm@gmail.com>
2011-11-30 12:52:18 +0900

    iphone login

--- app/helpers/basic_authentication.rb
+++ app/helpers/basic_authentication.rb
@@ -1,10 +1,1 @@
-module BasicAuthentication
-  def basic_authentication
-    unless current_user || Rails.env.test?
-      authenticate_or_request_with_http_basic do |username,password|
-        @user = User.authenticate(username,password)
-        session[:user_id] = @user.id if @user
-      end
-    end
-  end
-end
+../../../basiq/app/helper/basic_authentication.rb
